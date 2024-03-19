6845eb1bcd3ca59b9c276f4ddf5c5ed6096bd2e3
Luke_Xie <k718198@gmail.com>
2024-03-19 20:20:14 +0800

    init check data

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1344,6 +1344,8 @@ export default function ProcessPage() {
       // shapes.push(desicion_1);
       // shapes.push(curve);
 
+      checkData()
+
       requestAnimationFrame(draw);
     }
 
@@ -1493,7 +1495,7 @@ export default function ProcessPage() {
                             title={
                               <div className="flex">
                                 <p className="mr-1">{child.shape.title}</p>
-                                <svg
+                                {/* <svg
                                   className="w-6 h-6 text-gray-800 dark:text-white"
                                   aria-hidden="true"
                                   xmlns="http://www.w3.org/2000/svg"
@@ -1509,7 +1511,7 @@ export default function ProcessPage() {
                                     stroke-width="2"
                                     d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"
                                   />
-                                </svg>
+                                </svg> */}
                               </div>
                             }
                             // open={group.head.open}
