f0e1c107e4c5b42b5497c0136a0e997bf797889a
Luke <luke_xie@wistron.com>
2024-03-26 12:43:50 +0800

    bycrpt register password

--- backend/src/models/auth.ts
+++ backend/src/models/auth.ts
@@ -1,18 +1,25 @@
-import pool from '../db'
-import { RowDataPacket } from 'mysql2';
+import pool from "../db";
+import { RowDataPacket } from "mysql2";
 
 export default class Auth {
-
   // 檢查是否已存在相同的帳號
   async checkDuplicateAccountName(account: string) {
-    if (!pool) return
-    const [rows] = await pool.query('SELECT COUNT(*) AS count FROM user WHERE account = ?', [account]);
-    return (rows as RowDataPacket[])[0].count > 0
+    if (!pool) return;
+    const [
+      rows,
+    ] = await pool.query(
+      "SELECT COUNT(*) AS count FROM user WHERE account = ?",
+      [account]
+    );
+    return (rows as RowDataPacket[])[0].count > 0;
   }
 
   // 創建新會員
-  async createUser(account: string, email: string, password: string) {
-    await pool.query('INSERT INTO user (account, email, password) VALUES (?, ?, ?)', [account, email, password]);
+  async createUser(account: string, email: string, hash: string) {
+    await pool.query(
+      "INSERT INTO user (account, email, password) VALUES (?, ?, ?)",
+      [account, email, hash]
+    );
   }
 
   echo() {
--- backend/src/services/auth.ts
+++ backend/src/services/auth.ts
@@ -1,20 +1,24 @@
 import { Auth as AuthModel } from "../models";
-import validator from 'validator';
+import validator from "validator";
+import bcrypt from "bcrypt";
 
 export default class Auth {
   private authModel = new AuthModel();
 
   async register(account: string, email: string, password: string) {
     if (!validator.isEmail(email)) {
-      throw new Error('invalid email format.');
+      throw new Error("invalid email format.");
     }
 
     const isDuplicate = await this.authModel.checkDuplicateAccountName(account);
     if (isDuplicate) {
-      throw new Error('account already exists.');
+      throw new Error("account already exists.");
     }
 
-    await this.authModel.createUser(account, email, password);
+    bcrypt.hash(password, 10, async (err, hash) => {
+      // Store hash in your password DB.
+      await this.authModel.createUser(account, email, hash);
+    });
   }
 
   echo() {
