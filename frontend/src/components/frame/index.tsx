956f0e8cfe2111dcb80c63b604c9dde49d8183ec
Luke_Xie <claudia.sy.teng@viewsonic.com>
2024-07-14 22:01:58 +0800

    feat: re-layout dataFrame component

--- frontend/src/components/frame/index.tsx
+++ frontend/src/components/frame/index.tsx
@@ -1,16 +1,13 @@
 "use client";
-import { useEffect, useState } from "react";
-import { v4 as uuidv4 } from "uuid";
-import { Props, Selections } from "@/types/components/dataFrame";
-import { Title, DataItem, Data as DataType } from "@/types/shapes/common";
-import cloneDeep from "lodash/cloneDeep";
-import Data from "@/shapes/data";
 
 export default function Frame(props: any) {
-
   return (
     <div
+      key={props.key}
+      id={props.id}
+      style={props.style}
       className={`rounded-lg p-4 flex flex-col bg-white-500 shadow-md ${props.className}`}
+      onClick={props.onClick}
     >
       {props.children}
     </div>
