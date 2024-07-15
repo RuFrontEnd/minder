9f7087e2c6098d175489b819ead90dbf24f3d9f9
luke_xie@wistron.com <luke_xie@wistron.com>
2024-07-15 17:16:35 +0800

    feat: new X icon asset

--- frontend/src/components/modal/index.tsx
+++ frontend/src/components/modal/index.tsx
@@ -1,11 +1,13 @@
 import ReactModal from "react-modal";
 import CircleButton from "@/components/circularButton";
+import XIcon from "@/assets/svg/x.svg";
 import * as ModalTypes from "@/types/components/modal";
 import { tailwindColors } from "@/variables/colors";
 
 ReactModal.setAppElement("body");
 
 const Modal = (props: ModalTypes.Props) => {
+  console.log(tailwindColors.white["500"]);
   return (
     <ReactModal
       isOpen={props.isOpen}
@@ -35,22 +37,11 @@ const Modal = (props: ModalTypes.Props) => {
             className={"absolute top-[0px] right-[0px]"}
             onClick={props.onClickX}
             content={
-              <svg
-                className="w-3 h-3"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                width="12"
-                height="12"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke={`${tailwindColors.white["500"]}`}
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="4"
-                  d="M6 18 17.94 6M18 18 6.06 6"
-                />
-              </svg>
+              <XIcon
+                className={`w-3 h-3 [&>*]:stroke-cyan-700}`}
+                fill={tailwindColors.white["500"]}
+                storke={tailwindColors.white["500"]}
+              />
             }
           />
         )}
