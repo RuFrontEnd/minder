df01b59ee46c9c919c93d27aa8efcbdfdf44af8b
luke_xie@wistron.com <luke_xie@wistron.com>
2026-01-22 17:10:28 +0800

    feat: shape connection entity

---
+++ backendCSharp/Minder_backend/Domain/Entities/ShapeConnectoinEntity.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
+{
+    public enum Direciton
+    {
+        'left' = 1,
+        'top' = 2,
+        'right' = 3,
+        'bottom' = 4
+    }
+    public class ShapeConnecitonEntity
+    {
+        public Guid Id { get; private set; }
+
+        // FK
+        public Guid SourceId { get; set; }
+        [ForeignKey("ShapeId")]
+        public ShapeEntity Shape { get; set; } = null!;
+
+        // FK
+        public Guid TargetId { get; private set; }
+        public ShapeEntity Shape { get; set; } = null!;
+    }
+
+}
