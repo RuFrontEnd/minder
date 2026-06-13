35ae9ea9052c47f09482450fd7e3b4723367c1cb
Luke_Xie <k718198@gmail.com>
2026-06-13 15:15:27 +0800

    fix: remove console feature

--- frontend/src/sections/console/index.tsx
+++ frontend/src/sections/console/index.tsx
@@ -26,90 +26,44 @@ export default function Console(props: ConsoleTypes.Props) {
   };
 
   return (
-    <SidePanel
-      role={"console"}
-      open={props.isConsoleOpen}
-      flow={SidePanelTypes.Flow.column}
-      switchButtonD={SidePanelTypes.SwitchButtonD.start}
-      horizentalD={(() => {
-        if (props.isOverAllSidePanelOpen && props.isIndivisualSidePanelOpen)
-          return SidePanelTypes.HorizentalD.m;
+    <>
+      <div className="fixed bottom-4 inset-x-0 flex justify-center items-center">
+        <div className="relative inline-flex items-center">
+          <div className="absolute right-full mr-2 flex gap-2 items-center">
+            <div></div>
+            <div></div>
+          </div>
 
-        if (props.isOverAllSidePanelOpen) return SidePanelTypes.HorizentalD.r;
-
-        if (props.isIndivisualSidePanelOpen)
-          return SidePanelTypes.HorizentalD.l;
-
-        return SidePanelTypes.HorizentalD.m;
-      })()}
-      verticalD={SidePanelTypes.VerticalD.b}
-      w={(() => {
-        if (props.isOverAllSidePanelOpen && props.isIndivisualSidePanelOpen)
-          return "calc(100vw - 688px)";
-
-        if (props.isOverAllSidePanelOpen || props.isIndivisualSidePanelOpen)
-          return "calc(100vw - 344px)";
-
-        return "calc(100vw)";
-      })()}
-      h={"284px"}
-      onClickSwitch={() => {
-        props.setIsConsoleOpen((isOpenConsole: any) => !isOpenConsole);
-      }}
-    >
-      <div className="h-full p-4">
-        <p className="px-2 text-lg font-semibold text-grey-1">Console</p>
-        <Divider className="w-full" margin={{ y: 4 }} />
-        <ul>
-          {props.consoles.map((consoleItem: any) => {
-            const status = (() => {
-              switch (consoleItem.status) {
-                case CommonTypes.DataStatus.error:
-                  return "text-error-500";
-              }
-            })();
-            return (
-              <li
-                className={`px-2 mb-1 ${
-                  status || ""
-                } cursor-pointer underline-offset-2 hover:underline`}
-                onClick={() => {
-                  onClickMessage(consoleItem.shape.id);
-                }}
-              >
-                {consoleItem.message}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-      <div className="absolute -top-4 -translate-y-full left-1/2 -translate-x-1/2">
-        <CreateShapeButtons
-          isOverAllSidePanelOpen={props.isOverAllSidePanelOpen}
-          actionRecords={props.actionRecords}
-          shapes={props.shapes}
-          offset={props.offset}
-          scale={props.scale}
-          reload={props.reload}
-          initShapeSize={props.initShapeSize}
-        />
-      </div>
-      <div className="absolute -top-4 -translate-y-full right-0 flex">
-        <SquareButton
-          className="mr-4 border border-grey-5"
-          size={32}
-          content={
-            <Icon
-              type={IconTypes.Type.rotateCcw}
-              w={14}
-              h={14}
-              fill={tailwindColors.grey["1"]}
+          <div>
+            <CreateShapeButtons
+              isOverAllSidePanelOpen={props.isOverAllSidePanelOpen}
+              actionRecords={props.actionRecords}
+              shapes={props.shapes}
+              offset={props.offset}
+              scale={props.scale}
+              reload={props.reload}
+              initShapeSize={props.initShapeSize}
             />
-          }
-          onClick={onClickUndoButton}
-        />
-        <Zoom zoom={props.zoom} scale={props.scale} />
+          </div>
+
+          <div className="absolute left-full ml-2 flex gap-2 items-center">
+            <SquareButton
+              className="border border-grey-5"
+              size={32}
+              content={
+                <Icon
+                  type={IconTypes.Type.rotateCcw}
+                  w={14}
+                  h={14}
+                  fill={tailwindColors.grey["1"]}
+                />
+              }
+              onClick={onClickUndoButton}
+            />
+            <Zoom zoom={props.zoom} scale={props.scale} />
+          </div>
+        </div>
       </div>
-    </SidePanel>
+    </>
   );
 }
