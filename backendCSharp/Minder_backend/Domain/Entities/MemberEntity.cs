070aff011ecb011cb2e2e2e2a9bc0a9a071381ff
Luke_Xie <k718198@gmail.com>
2026-06-29 19:54:25 +0800

    feat: new member entity

---
+++ backendCSharp/Minder_backend/Domain/Entities/MemberEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Entities
+{
+    public class MemberEntity
+    {
+        public MemberEntity(Guid userId, Guid projectId)
+        {
+            Id = Guid.NewGuid();
+            UserId = userId;
+            ProjectId = projectId;
+        }
+        public Guid Id { get; private set; }
+        public Guid UserId { get; private set; }
+        public Guid ProjectId { get; private set; }
+    }
+
+}
---
+++ backendCSharp/Minder_backend/Domain/Repositories/IMemberRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+
+namespace Domain.Repositories
+{
+    public interface IMemberRepository
+    {
+        Task<IEnumerable<MemberEntity>> GetMembersAsync(Guid projectId);
+        Task<IEnumerable<MemberEntity>> InsertMembersAsync(Guid projectId, Guid userId);
+        Task<IEnumerable<MemberEntity>> RemoveMemberAsync(Guid projectId, Guid userId);
+    }
+
+}
